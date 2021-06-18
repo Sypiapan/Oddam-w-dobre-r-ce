965194f5d296caa11676076a7c148c59005cd04e
sypiapan <sypiapan@gmail.com>
2021-06-18 08:55:31 +0200

    third commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,34 @@
+
+
 export default function Contact() {
     return (
 
+        <>
+            <div className="contact">
 
-        <h3 id="contact"> Contact</h3>
+
+                <div className="contact_content">
+                    <p className="contact_title">Skontaktuj się z nami</p>
+                    <div className="head_icon"></div>
+
+                </div>
+                <div className="form__field">
+                    <label className="form__label" htmlFor="name">Name</label>
+                    <input type="text" className="form__input name" id="name"/>
+                </div>
+                <div className="form__field">
+                    <label className="form__label" htmlFor="email">E-mail</label>
+                    <input type="email" className="form__input email" id="email"/>
+                </div>
+                <div className="form__field">
+                    <label className="form__label" htmlFor="email">E-mail</label>
+                    <textarea  className="form__input text" id="text"/>
+                </div>
+                <button>Wyslij</button>
+
+
+            </div>
+
+        </>
     );
 }
--- src/components/Organisation.js
+++ src/components/Organisation.js
@@ -1,8 +1,20 @@
+import Logo from "../assets/Icon-1.svg";
+import Logo2 from "../assets/Icon-2.svg";
+import Logo3 from "../assets/Icon-3.svg";
+import Logo4 from "../assets/Icon-4.svg";
+
 export default function Organisation() {
     return (
+        <main className="page_second-content">
+            <h2 className="article-header">Komu pomagamy</h2>
+            <div className="head_icon"></div>
 
+            <div className="page_second_buttons">
+            <div className="page_second_buttons_item">Fundacjom</div>
+            <div className="page_second_buttons_item">Organizacjom pozarządowym</div>
+            <div className="page_second_buttons_item">Lokalnym zbiórkom</div>
+        </div>
 
-        <h3 id="organisation
-        "> Organisation</h3>
-    );
+</main>
+);
 }
